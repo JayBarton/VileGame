8a448f9b20b3d724bd53854fcde2c0f2f2dd49d4
JayBarton <dougssteele1@gmail.com>
2019-11-18 04:16:22 -0500

    UE4 Project created

---
+++ VileGame/Source/VileGame/VileGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VileGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VILEGAME_API AVileGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
